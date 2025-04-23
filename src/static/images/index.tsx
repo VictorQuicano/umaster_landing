61a76030302ea7438c374805c57b90c1abdf3616
Alejandro Quicaño <alejandroxd25k@gmail.com>
2025-04-23 00:28:43 -0500

    init: Change in home page

--- src/static/images/index.tsx
+++ src/static/images/index.tsx
@@ -3,26 +3,26 @@ import PreviewPng from "./preview.webp";
 import HeroImg from "./hero-img.webp";
 import Logo from "./logo.webp";
 
-import HeroSlider1 from "./hero-slider-01.webp";
-import HeroSlider2 from "./hero-slider-02.webp";
-import HeroSlider3 from "./hero-slider-03.webp";
+import HeroSlider1 from "./slider_1.webp";
+import HeroSlider2 from "./slider_2.webp";
+import HeroSlider3 from "./slider_3.webp";
 
 import InfiniteImg1 from "./infinite-img-1.webp";
 import InfiniteImg2 from "./infinite-img-2.webp";
 
 export const images = {
-    preview: PreviewPng,
-    heroImg: HeroImg,
-    logo: Logo,
+  preview: PreviewPng,
+  heroImg: HeroImg,
+  logo: Logo,
 };
 
 export type ImageProps = {
-    srcLocal?: keyof typeof images;
-    src?: string;
-    alt: string;
-    width?: number | string;
-    height?: number | string;
-    loading?: "lazy" | "eager";
+  srcLocal?: keyof typeof images;
+  src?: string;
+  alt: string;
+  width?: number | string;
+  height?: number | string;
+  loading?: "lazy" | "eager";
 };
 
 /**
@@ -30,44 +30,44 @@ export type ImageProps = {
  * <Image src="logo" alt="logo" width="50px" height="50px" />
  */
 export const Image: FC<ImageProps> = ({
-    alt,
-    srcLocal,
-    height,
-    width,
-    src,
-    loading,
-    ...rest
+  alt,
+  srcLocal,
+  height,
+  width,
+  src,
+  loading,
+  ...rest
 }) => {
-    /**
-     * If srcLocal or src is not provided throw an error
-     */
-    if (!srcLocal && !src) {
-        throw new Error("srcLocal or src is required");
-    }
+  /**
+   * If srcLocal or src is not provided throw an error
+   */
+  if (!srcLocal && !src) {
+    throw new Error("srcLocal or src is required");
+  }
 
-    /**
-     * this component should be able to use local images or images from external sources
-     */
-    const image = srcLocal ? images[srcLocal] : { src, width, height };
+  /**
+   * this component should be able to use local images or images from external sources
+   */
+  const image = srcLocal ? images[srcLocal] : { src, width, height };
 
-    return (
-        <img
-            src={image.src}
-            alt={alt}
-            width={width ? width : image.width}
-            height={height ? height : image.height}
-            loading={loading}
-            {...rest}
-        />
-    );
+  return (
+    <img
+      src={image.src}
+      alt={alt}
+      width={width ? width : image.width}
+      height={height ? height : image.height}
+      loading={loading}
+      {...rest}
+    />
+  );
 };
 
 // default export of the images
 export {
-    PreviewPng,
-    HeroSlider1,
-    HeroSlider2,
-    HeroSlider3,
-    InfiniteImg1,
-    InfiniteImg2,
+  PreviewPng,
+  HeroSlider1,
+  HeroSlider2,
+  HeroSlider3,
+  InfiniteImg1,
+  InfiniteImg2,
 };
